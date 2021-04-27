6be5733894cdc6f44f8f6bf802148d396f469320
Bernardo Mayol <bernardomayol@yahoo.com>
2021-04-26 18:14:38 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public enum DayOfWeek
+    {
+        Monday=1,
+        Tuesday=2,
+        Wednesday=3,
+        Thursday=4,
+        Friday=5,
+        Saturday=6,
+        Sunday=7
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayOfWeek currentDay = DayOfWeek.Monday;
+
+            Console.WriteLine(currentDay);
+        }
+    }
+}
